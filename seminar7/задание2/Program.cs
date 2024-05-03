2d81701098af635ea3db63c71eec762d48d2735d
Анна <87242449+anna20201@users.noreply.github.com>
2024-05-03 17:23:17 +0300

    new

---
+++ seminar7/задание2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        Console.Write("Введите неотрицательное число m: "); 
+        int m = int.Parse(Console.ReadLine()); 
+ 
+        Console.Write("Введите неотрицательное число n: "); 
+        int n = int.Parse(Console.ReadLine()); 
+ 
+        int result = AckermannFunction(m, n); 
+        Console.WriteLine($"Значение функции Аккермана для ({m}, {n}) равно: {result}"); 
+    } 
+ 
+    static int AckermannFunction(int m, int n) 
+    { 
+        if (m == 0) 
+            return n + 1; 
+        else if (n == 0) 
+            return AckermannFunction(m - 1, 1); 
+        else 
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1)); 
+    } 
+}
---
+++ seminar7/задание3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        int[] myArray = { 10, 20, 30, 40, 50 }; 
+ 
+        Console.WriteLine("Элементы массива, начиная с конца:"); 
+        PrintArrayReverse(myArray, myArray.Length - 1); 
+    } 
+ 
+    static void PrintArrayReverse(int[] arr, int index) 
+    { 
+        if (index >= 0) 
+        { 
+            Console.Write(arr[index] + " "); 
+            PrintArrayReverse(arr, index - 1); 
+        } 
+    } 
+}
